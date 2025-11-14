e0d1a23446c36463348bcb33e3b801e87318aab4
m3NORY <v444amelibbt@gmail.com>
2025-11-15 00:11:18 +0200

    134

--- labb 8/build/cmake_install.cmake
+++ labb 8/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Git/school123/labb 8
+# Install script for directory: C:/Git/uni/labb 8
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/labb6")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/labb8")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Git/school123/labb 8/build/install_local_manifest.txt"
+  file(WRITE "C:/Git/uni/labb 8/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Git/school123/labb 8/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Git/uni/labb 8/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- labb 8/main.cpp
+++ labb 8/main.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
+#include <fstream>
 #include "sport_res.hpp"
 
+void createTestInput() {
+    std::ofstream file("input.txt");
+    file << "4\n";
+    file << "Иванов 45.3\n";
+    file << "Петров 42.8\n";
+    file << "Сидоров 48.1\n";
+    file << "Козлов 44.5\n";
+    file.close();
+    std::cout << "Создан тестовый файл input.txt" << std::endl;
+}
+
 int main() {
+    std::cout << "=== ЗАПУСК ПРОГРАММЫ ===" << std::endl;
+    
     const std::string inputFile = "input.txt";
     const std::string outputFile = "output.txt";
-    int runnerCount = 0;
     
+    // Проверяем есть ли файл, если нет - создаем
+    std::ifstream test(inputFile);
+    if (!test.is_open()) {
+        std::cout << "Файл input.txt не найден. Создаю..." << std::endl;
+        createTestInput();
+    } else {
+        test.close();
+        std::cout << "Файл input.txt найден!" << std::endl;
+    }
+    
+    int runnerCount = 0;
     Runner* runners = readRunnersFromFile(inputFile, runnerCount);
     
     if (runners == nullptr) {
-        std::cout << "Ошибка" << std::endl;
+        std::cout << "Ошибка чтения файла!" << std::endl;
         return 1;
     }
     
@@ -18,7 +42,8 @@ int main() {
     
     delete[] runners;
     
-    std::cout << "Все!" << std::endl;
+    std::cout << "=== ПРОГРАММА ЗАВЕРШЕНА ===" << std::endl;
+    std::cout << "Проверь файл output.txt!" << std::endl;
+    
     return 0;
 }
-// всем пока
